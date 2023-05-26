b8fc8611fe3116c0178347be4ea7ba653a4853cf
Alex Saday <alexeykildyushev@yandex.ru>
2023-05-26 09:03:23 +0300

    first commit

---
+++ Snake/DataBindings.cs
@@ -0,0 +1,7 @@
+﻿namespace Snake;
+
+public class DataBindings
+{
+    public string UserName { get;set; } = string.Empty;
+    public int Score { get;set; } 
+}
---
+++ Snake/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            panel1 = new Panel();
+            snakeTimer = new System.Windows.Forms.Timer(components);
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panel1.Location = new Point(14, 16);
+            panel1.Margin = new Padding(3, 4, 3, 4);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(569, 689);
+            panel1.TabIndex = 0;
+            panel1.SizeChanged += panel1_SizeChanged;
+            panel1.Click += panel1_Click;
+            panel1.Paint += panel1_Paint;
+            // 
+            // snakeTimer
+            // 
+            snakeTimer.Interval = 1000;
+            snakeTimer.Tick += snakeTimer_Tick;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            label1.AutoSize = true;
+            label1.Location = new Point(590, 16);
+            label1.Name = "label1";
+            label1.Size = new Size(47, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Очки:";
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            label2.AutoSize = true;
+            label2.Location = new Point(590, 36);
+            label2.Name = "label2";
+            label2.Size = new Size(17, 20);
+            label2.TabIndex = 2;
+            label2.Text = "0";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(674, 721);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(panel1);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form1";
+            Text = "Змейка";
+            Load += Form1_Load;
+            KeyDown += Form1_KeyDown;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private System.Windows.Forms.Timer snakeTimer;
+        private Label label1;
+        private Label label2;
+    }
+}
